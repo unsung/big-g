0bb3d7e8fad9b9fae540c07b14278050a1eb1e40
Josh Manning <unsungmaster@gmail.com>
2015-03-09 17:49:48 -0700

    added time visualisation

--- big-g.py
+++ big-g.py
@@ -1,26 +1,21 @@
 import numpy as np
+import cv2
 from data import Data
 
+const = 1.0107
 LEN = 640.*1167./1280.
 C = LEN/1.325
 D = 1.157*C
 pi = np.pi
-r = 0.05
+r = 0.045
 d = 0.05
 M = 1.438
-L=4.235
+L = 4.235
 S = 0
 T0 = 0
     
-def angle(x):
-    return np.arctan(x/D)
-
-class Big_G(object):
-    def __init__(self):
-        pass
-
-    def run(self):
-        return pi*pi*r*r*d*S/(M*T0*T0*L)
+def find_g():
+    return pi*pi*r*r*d*S/(M*T0*T0*L)
 
 
 
@@ -32,8 +27,34 @@ if __name__ == '__main__':
         pathr = sys.argv[2]
     except: exit()
 
-    _,Bl,Tl,_ = Data(pathl).run()
-    _,Br,Tr,_ = Data(pathr).run()
+    valsl, sdevl, xl,tl, valsl2, sdevl2 = Data(pathl).run()
+    valsr, sdevr, xr,tr, valsr2, sdevr2 = Data(pathr).run()
+    """
+    valsl, sdevl, valsl2, sdevl2 = Data(pathl).run()
+    valsr, sdevr, valsr2, sdevr2 = Data(pathr).run()
+    """
+    _,Bl, Tl,_ = valsl
+    _,Br, Tr,_ = valsr
+    _,Bl2, Tl2,_,bl2 = valsl2
+    _,Br2, Tr2,_,br2 = valsr2
+    _,sBl, sTl,_ = sdevl
+    _,sBr, sTr,_ = sdevr
+    _,sBl2, sTl2,_,sbl2 = sdevl2
+    _,sBr2, sTr2,_,sbr2 = sdevr2
     S = np.fabs(Bl-Br)/C
-    T0 = 0.5*(Tl+Tr)
-    print Big_G().run()
+    T0 = Tl
+    tmax=np.amax([tl[-1],tr[-1]])
+    xmin=np.amin([np.amin(xl), np.amin(xr)])
+    xmax=np.amax([np.amax(xl), np.amax(xr)])
+    plot = np.zeros((int(xmax-xmin), int(tmax), 3), np.uint8)
+    print plot.shape
+    for i in np.arange(xl.size-1):
+        plot[np.clip(int(xl[i]-xmin),0,plot.shape[0]-1),int(tl[i])]=(0,0,255)
+    for i in np.arange(xr.size-1):
+        plot[np.clip(int(xr[i]-xmin),0,plot.shape[0]-1),int(tr[i])]=(0,255,0)
+
+    cv2.namedWindow('wave')
+    cv2.imshow('wave',plot)
+    print find_g()
+    while cv2.waitKey(0)&0xFF is not 27:
+        pass
--- data.py
+++ data.py
@@ -4,9 +4,16 @@ import os
 from scipy.optimize import curve_fit
 from scipy.signal import gaussian
 
-LEN = 640
+LEN = 640.*1167./1280.
 C = LEN/1.325
 D = 1.157*C
+pi = np.pi
+r = 0.045
+d = 0.05
+M = 1.438
+L = 4.235
+m = 0.015
+I = 2*m*d*d
 dy = 5
     
 def angle(x):
@@ -29,6 +36,7 @@ class Data(object):
         self.done = False
         self.img = None
         self.showimg = None
+        self.calcimg = None
         self.data = None
         self.starttime = None
         self.currtime = None
@@ -36,22 +44,31 @@ class Data(object):
     def hrange(self):
         return np.arange(self.h-dy, self.h+dy+1)
 
-    def gendata(self):
+    def gendata(self,start=320,again=True):
         self.w = self.img.shape[1]
         self.y = self.img[self.hrange(),:,2].sum(axis=0)/(2.*dy)
         plot = np.zeros((256, self.w, 3), np.uint8)
         for i in range(self.w):
             plot[255-self.y[i], i]=(0,0,255)
-        cv2.imshow('data', plot)
+        self.calcimg = np.copy(plot)
         if self.analyzing:
             params = None
             try:
-                params = self.fit()[0]
+                params = self.fit(start)[0]
                 params = np.fabs(params)
-                if params[1] > 10: params = None
+                if self.data is not None and np.fabs((params[0]-LEN/2-self.data[-1][1])/(self.currtime-self.data[-1][0])*5) > 100:
+                    params = None;
+
+                if params[1] > 20:
+                    params = None
+
+                if params[2] / (params[1] * np.sqrt(2 * np.pi)) < 30:
+                    params = None
+
             except: return
 
             if params is not None:
+                #print params
                 new = [[
                         self.currtime,
                         params[0]-LEN/2,
@@ -61,6 +78,16 @@ class Data(object):
                     self.data = np.append(self.data, new, axis=0)
                 else:
                     self.data = np.array(new)
+                for x in np.arange(640):
+                    self.calcimg[255-self.gauss(x,params[0],params[1],params[2]),x]=(0,255,0)
+                cv2.imshow('data', self.calcimg)
+            else:
+                if(again):
+                    self.gendata(0,False)
+                    self.gendata(self.w,False)
+                    cv2.imshow('data', self.calcimg)
+                else:
+                    return
 
     def dispimg(self):
         self.showimg = np.copy(self.img)
@@ -87,16 +114,17 @@ class Data(object):
     def onmousedata(self, event, x, y, flags, param):
         pass
 
-    def gauss(self, x, u, s, A, off):
-        return off + A * np.power(s * np.sqrt(2 * np.pi), 2.) *\
+    #def gauss(self, x, u, s, A, off):
+    def gauss(self, x, u, s, A):
+        return np.mean(self.y) + A / (s * np.sqrt(2 * np.pi)) *\
             np.exp(-0.5 * np.power((x - u) / s, 2.))
 
-    def fit(self):
+    def fit(self,start):
         return curve_fit(
                 self.gauss,
                 range(self.w),
                 self.y,
-                p0=[self.w/2,50,50,50]
+                p0=[start,10,1000]
                 )
 
     def sin(self, t, A, B, T, d):
@@ -105,6 +133,13 @@ class Data(object):
     def calcsin(self):
         t,x,s= self.data.T
         return curve_fit(self.sin, t, x, p0 = [1,0,600,0], sigma = s, absolute_sigma=True)
+
+    def dampsin(self, t, A, B, T, d, b):
+        return A*np.exp(-b*t/(2*I))*np.sin(2.*np.pi*t/T + d) + B
+
+    def calcdampsin(self):
+        t,x,s= self.data.T
+        return curve_fit(self.dampsin, t, x, p0 = [1,0,600,0,.11], sigma = s, absolute_sigma=True)
 
     def run(self):
         for imgname in sorted(os.listdir(self.path)):
@@ -138,8 +173,13 @@ class Data(object):
         while cv2.waitKey(0)&0xFF is not 27:
             pass
 
+        t,x,s = self.data.T
         cv2.destroyAllWindows()
-        return self.calcsin()[0]
+        vals,cov = self.calcsin()
+        vals2,cov2 = self.calcdampsin()
+        sdev = np.sqrt(np.clip(np.diag(cov),0,9999))
+        sdev2 = np.sqrt(np.clip(np.diag(cov2),0,9999))
+        return (vals,sdev,x,t,vals2,sdev2)
 
 if __name__ == '__main__':
     import sys
